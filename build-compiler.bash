d2f38e7b21ef850e0a1f20f4790107fc30cbae69
Greg Osuri <gosuri@gmail.com>
2014-11-24 02:02:21 +0000

    copying compiled source

--- build-compiler.bash
+++ build-compiler.bash
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+RUNTIME_PACKAGES=""
+
 function run() {
   # create cache directory if missing
   if [[ ! -d "${cachedir}" ]]; then
@@ -11,7 +13,7 @@ function run() {
 
   # fetch the new app code
   get_app_source
-  build_docker_image
+  build_compiler_image
   # archive
 }
 
@@ -22,10 +24,13 @@ function clean() {
 }
 
 function get_app_source() {
-  git clone ${repo} ${cachedir}/app --depth 1
+  info "fetching application source"
+  git clone ${repo} ${cachedir}/app --depth 1 2> ${logfile}
+  log "successfully cloned source to ${cachedir}/app"
 }
 
-function build_docker_image() {
+function build_compiler_image() {
+  info "compiling application"
   local image="app-base"
   tee "${cachedir}/Dockerfile" > /dev/null <<EOF
 FROM ${image}
@@ -34,7 +39,22 @@ ENV RAILS_ENV production
 RUN bundle install --path=vendor/bundle --binstubs vendor/bundle/bin  --jobs=4 --retry=3
 RUN bundle exec rake assets:precompile
 EOF
-  docker build --tag app-compiler $cachedir
+  log "building compiler image using ${cachedir}/Dockerfile"
+  docker build --tag app-compiler $cachedir &> ${logfile}
+
+  local id=$(docker run -dt app-compiler /bin/bash)
+  docker cp ${id}:/app compiled
+  debug "application compiled to ${cachedir}/compiled"
+  debug "removing compiler container"
+  docker --force rm ${id} 
+}
+
+function build_runtime_image() {
+ cat > "${cachedir}/runtimeimage" <<EOF
+FROM ubuntu
+ENV RAILS_ENV production
+ADD app app
+EOF
 }
 
 function help() {
@@ -50,15 +70,38 @@ function validate() {
   fi
 }
 
+function info() {
+  local msg="==> build-compiler: ${*}"
+  echo -e "${msg}" >> ${logfile}
+  echo_info "${msg}"
+}
+
+function log() {
+  local msg="                    ${*}"
+  echo -e "${msg}"
+  echo -e "${msg}" >> ${logfile}
+}
+
+function debug() {
+  [ "${DEBUG}" == "true" ] && log ${*}
+}
+
 function init() {
+  debug "using logfile ${logfile}"
   if [[ -z "${cachedir}" ]]; then
-    cachedir=${TMPDIR}/build-compiler-$$.$RANDOM
+    cachedir=$(mktemp -d --tmpdir $TMPDIR build-compiler-XXXXXXX)
   fi
+  debug "using cache directory ${cachedir}"
   bash_sugar_init || exit 2
 }
 
 repo=${1}
 cachedir=${2}
+logfile=$(mktemp --tmpdir $TMPDIR build-compiler-XXXXXXX.log)
+
+if [ -z "${DEBUG}" ]; then
+  DEBUG=false
+fi
 
 init
 validate
